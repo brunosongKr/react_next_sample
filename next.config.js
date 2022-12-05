cc0da1169eadad04abf9d2a4912d3bcb18d95b2a
BrunoSong <brunosong@wjtb.net>
2022-12-05 15:30:19 +0900

    sample

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env : {
+    monggodb_username: 'brunosong',
+    monggodb_password: '1234'
+  }
 }
 
 module.exports = nextConfig
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+
+const connectionString = `${process.env.monggodb_username}`;
+
 type Data = {
   name: string
+  connectionString : string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' ,connectionString : connectionString })
 }
